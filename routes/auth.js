3e47f953a598d36c177fddca8b15b1e3f16e5ee7
Nikolas Brandt <buddercreeperman@gmail.com>
2022-02-26 15:42:52 -0800

    Some auth work

--- routes/auth.js
+++ routes/auth.js
@@ -2,29 +2,48 @@
 
 import { Router } from 'express';
 import db from '../utils/database.js';
-// import user from '../models/user.js';
+import user from '../models/user.js';
 
 const router = Router();
 
 router.post('/register', (req, res, next) => {
-    // create user
-    res.status(201);
-
+    user.createNewUser(req.body.email, req.body.username, req.body.password, req.body.bio).then(success => {
+        if (!success) {
+            res.status(406);
+            res.send('User already exists.');
+        }
+        
+        res.status(201);
+        res.send('OK.');
+    }).catch(err => {
+        next(err);
+    });
 });
 
 router.post('/login', (req, res, next) => {
-    if (true /*login failed*/) {
-        res.status(401);
-        res.send('Invalid username or password');
-    } else {
-        db.createSession(/* user id */).then(session => {
+    verifyUser(req.body.email, req.body.password).then(success => {
+        if (!success) {
+            res.status(401);
+            res.send('Invalid username or password');
+            return;
+        }
+
+        let user = findUser(req.body.email);
+
+        if (!user) {
+            res.status(500);
+            res.send('Could not find user');
+            return;
+        }
+
+        db.createSession(user.id).then(session => {
             res.status(200);
             res.send({ session });
         }).catch(err => {
             res.status(504);
             next(err);
         });
-    }
+    });
 });
 
 export default router;
